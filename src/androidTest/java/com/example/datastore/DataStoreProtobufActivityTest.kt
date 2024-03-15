5589d732236ad078bcff26d75c375842d091adb0
Victor Shpyrka <victor.shpyrka@gmail.com>
2024-03-15 10:42:52 +0200

    fix: Android test fix

--- src/androidTest/java/com/example/datastore/DataStoreProtobufActivityTest.kt
+++ src/androidTest/java/com/example/datastore/DataStoreProtobufActivityTest.kt
@@ -1,6 +1,7 @@
 package com.example.datastore
 
 import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
@@ -24,22 +25,28 @@ class DataStoreProtobufActivityTest {
             onView(withId(R.id.status)).check(matches(withText("Cleared")))
             onView(withId(R.id.user_name)).perform(typeText("Hello"))
             onView(withId(R.id.user_email)).perform(typeText("World"))
+            Espresso.closeSoftKeyboard()
             onView(withId(R.id.save)).perform(click())
             onView(withId(R.id.status)).check(matches(withText("Saved")))
+            Espresso.closeSoftKeyboard()
             onView(withId(R.id.load_old_prefs)).perform(click())
             onView(withId(R.id.status)).check(matches(withText("Loaded")))
             onView(withId(R.id.user_name)).check(matches(withText("Hello")))
             onView(withId(R.id.user_email)).check(matches(withText("World")))
+            Espresso.closeSoftKeyboard()
             onView(withId(R.id.clear)).perform(click())
             onView(withId(R.id.load)).perform(click())
             onView(withId(R.id.user_name)).check(matches(withText("Hello")))
             onView(withId(R.id.user_email)).check(matches(withText("World")))
+            Espresso.closeSoftKeyboard()
             onView(withId(R.id.clear)).perform(click())
             onView(withId(R.id.status)).check(matches(withText("Cleared")))
             onView(withId(R.id.user_name)).perform(typeText("World"))
             onView(withId(R.id.user_email)).perform(typeText("Hello"))
+            Espresso.closeSoftKeyboard()
             onView(withId(R.id.save_datastore)).perform(click())
             onView(withId(R.id.status)).check(matches(withText("Saved")))
+            Espresso.closeSoftKeyboard()
             onView(withId(R.id.load)).perform(click())
             onView(withId(R.id.status)).check(matches(withText("Loaded")))
             onView(withId(R.id.user_name)).check(matches(withText("World")))
